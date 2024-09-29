de38930a5721dd29b1d20b55cb7411764c4afe50
Mackenzie Amoako <amoak017@umn.edu>
2024-09-29 12:54:00 -0500

    Finished Todo.list.component.ts

--- client/src/app/todo-list/todo-list.component.ts
+++ client/src/app/todo-list/todo-list.component.ts
@@ -1,6 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, computed} from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TodoService } from '../todo.service';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -13,4 +16,58 @@ export class TodoListComponent {
   private todoService = inject(TodoService);
   private snackBar = inject(MatSnackBar);
 
+  todoOwner = signal<string | undefined>(undefined);
+  todoStatus = signal<boolean | undefined>(undefined);
+  todoBody = signal<string | undefined>(undefined);
+  todoCategory = signal<string | undefined>(undefined);
+
+  viewType = signal<'card' | 'list'>('card');
+
+  errMsg = signal<string | undefined>(undefined);
+
+  private todoOwner$ = toObservable(this.todoOwner);
+  private todoCategory$ = toObservable(this.todoCategory);
+
+  serverFilteredTodos =
+
+  toSignal(
+    combineLatest([this.todoOwner$, this.todoCategory$]).pipe(
+      // `switchMap` maps from one observable to another. In this case, we're taking `role` and `age` and passing
+      // them as arguments to `userService.getUsers()`, which then returns a new observable that contains the
+      // results.
+      switchMap(([owner, category]) =>
+        this.todoService.getTodos({
+          owner,
+          category,
+        })
+      ),
+      catchError((err) => {
+        if (err.error instanceof ErrorEvent) {
+          this.errMsg.set(
+            `Problem in the client – Error: ${err.error.message}`
+          );
+        } else {
+          this.errMsg.set(
+            `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`
+          );
+        }
+        this.snackBar.open(this.errMsg(), 'OK', { duration: 6000 });
+        // `catchError` needs to return the same type. `of` makes an observable of the same type, and makes the array still empty
+        return of<Todo[]>([]);
+      }),
+      // Tap allows you to perform side effects if necessary
+      tap(() => {
+        // A common side effect is printing to the console.
+        // You don't want to leave code like this in the
+        // production system, but it can be useful in debugging.
+        // console.log('Users were filtered on the server')
+      })
+    )
+    filteredTodos = computed(() => {
+      const serverFilteredTodos = this.serverFilteredTodos();
+      return this.todoService.filterTodos(serverFilteredTodos, {
+        owner: this.todoOwner(),
+        category: this.todoCategory(),
+      });
+    });
 }
--- client/src/app/todo.service.ts
+++ client/src/app/todo.service.ts
@@ -1,9 +1,89 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Todo } from './todo';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  readonly todoUrl: string = `${environment.apiUrl}todos`;
+  private readonly ownerKey = 'owner';
+  private readonly statusKey = 'status';
+  private readonly bodyKey = 'body';
+  private readonly categoryKey = 'category';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getTodos(filters?: { owner?: TodoOwner; status?: boolean; body?: string; category?: string}): Observable<User[]> {
+    // `HttpParams` is essentially just a map used to hold key-value
+    // pairs that are then encoded as "?key1=value1&key2=value2&…" in
+    // the URL when we make the call to `.get()` below.
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.owner) {
+        httpParams = httpParams.set(this.ownerKey, filters.owner);
+      }
+      if (filters.status) {
+        httpParams = httpParams.set(this.statusKey, filters.status);
+      }
+      if (filters.body) {
+        httpParams = httpParams.set(this.bodyKey, filters.body);
+      }
+      if (filters.category) {
+        httpParams = httpParams.set(this.categoryKey, filters.category);
+    }
+    // Send the HTTP GET request with the given URL and parameters.
+    // That will return the desired `Observable<User[]>`.
+    return this.httpClient.get<Todo[]>(this.todoUrl, {
+      params: httpParams,
+    });
+  }
+}
+/**
+   * Get the `User` with the specified ID.
+   *
+   * @param id the ID of the desired user
+   * @returns an `Observable` containing the resulting user.
+   */
+getTodoById(id: string): Observable<Todo> {
+  // The input to get could also be written as (this.userUrl + '/' + id)
+  return this.httpClient.get<Todo>(`${this.todoUrl}/${id}`);
+}
+/**
+   * A service method that filters an array of `User` using
+   * the specified filters.
+   *
+   * Note that the filters here support partial matches. Since the
+   * matching is done locally we can afford to repeatedly look for
+   * partial matches instead of waiting until we have a full string
+   * to match against.
+   *
+   * @param users the array of `Users` that we're filtering
+   * @param filters the map of key-value pairs used for the filtering
+   * @returns an array of `Users` matching the given filters
+   */
+filterTodos(users: Todo[], filters: { owner?: string; category?: string }): Todo[] { // skip: JS-0105
+  let filteredTodos = todos;
+
+  // Filter by name
+  if (filters.owner) {
+    filters.owner = filters.owner.toLowerCase();
+    filteredTodos = filteredTodos.filter(todo => todo.owner.toLowerCase().indexOf(filters.owner) !== -1);
+  }
+
+  // Filter by company
+  if (filters.category) {
+    filters.category = filters.category.toLowerCase();
+    filteredTodos = filteredTodos.filter(todo => todo.category.toLowerCase().indexOf(filters.category) !== -1);
+  }
+
+  return filteredTodos;
+}
+
+addTodo(newTodo: Partial<Todo>): Observable<string> {
+  // Send post request to add a new user with the user data as the body.
+  return this.httpClient.post<{id: string}>(this.todoUrl, newTodo).pipe(map(res => res.id));
+}
 }
